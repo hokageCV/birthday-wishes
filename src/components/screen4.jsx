3c9bbcf074183ef596698ac020693b89f7504f29
hokageCV <chaitanyavaru@gmail.com>
2024-01-21 00:26:39 +0530

    Add birthday video

--- src/components/screen4.jsx
+++ src/components/screen4.jsx
@@ -1,7 +1,11 @@
+import birthdayAudio from '../assets/birthday.mp3'
+import birthdayVideo from '../assets/birthdayVideo.mp4'
 export default function Screen4() {
     return (
         <div className='screen-container gift-screen'>
             <h1>birthday music begins </h1>
+            <audio src={birthdayAudio} autoPlay />
+            <video src={birthdayVideo} autoPlay />
         </div>
     );
 }

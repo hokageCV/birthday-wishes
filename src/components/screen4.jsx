f40b8b58988f8f5e2ae04600ec25fe6fd6fe03b2
hokageCV <chaitanyavaru@gmail.com>
2024-01-21 01:38:52 +0530

    Add glow effect

--- src/components/screen4.jsx
+++ src/components/screen4.jsx
@@ -3,7 +3,7 @@ import birthdayVideo from '../assets/birthdayVideo.mp4'
 export default function Screen4() {
     return (
         <div className='screen-container gift-screen'>
-            <h1 className='text-blur-bg'>Happy Birthday Suhaan </h1>
+            <h1 className='text-blur-bg glow'>Happy Birthday Suhaan </h1>
             <audio src={birthdayAudio} autoPlay />
             <video src={birthdayVideo} autoPlay />
         </div>

aab360f82b3f220d85c8f2a1f4e6dcc10935f0f7
hokageCV <chaitanyavaru@gmail.com>
2024-01-21 02:30:06 +0530

    Fix video attributes

--- src/components/screen4.jsx
+++ src/components/screen4.jsx
@@ -1,11 +1,13 @@
-import birthdayAudio from '../assets/birthday.mp3'
-import birthdayVideo from '../assets/birthdayVideo.mp4'
+import birthdayAudio from '../assets/birthday.mp3';
+import birthdayVideo from '../assets/birthdayVideo.mp4';
 export default function Screen4() {
     return (
-        <div className='screen-container gift-screen'>
+        <div className='screen-container media-screen'>
             <h1 className='text-blur-bg glow'>Happy Birthday Suhaan </h1>
             <audio src={birthdayAudio} autoPlay />
-            <video src={birthdayVideo} autoPlay />
+            <div className='video-div'>
+                <video src={birthdayVideo} autoPlay muted loop />
+            </div>
         </div>
     );
 }
